98cd9503dbb62c972072f8a7a6d77d9db7eb1140
ozan isgor <ozanisgor11@gmail.com>
2022-09-30 15:54:23 +0300

    add: nodemon

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 const http = require('http');
-
-// Create a local server to receive data from
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(
