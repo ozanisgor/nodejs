15cc97cd56673f5d363ad62c85d7a5f26575f4cf
ozan isgor <ozanisgor11@gmail.com>
2022-09-30 14:47:43 +0300

    create first server

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-for (let index = 0; index < 10; index++) {
-  console.log(index);
-}
+const http = require('http');
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+  res.end(
+    JSON.stringify({
+      data: 'Hello Node!'
+    })
+  );
+});
+
+server.listen(3000);
