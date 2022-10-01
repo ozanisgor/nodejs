68fc5333e02c17bfe93271f09fc77cb9cc0b2a34
ozan isgor <ozanisgor11@gmail.com>
2022-10-01 11:56:06 +0300

    add: route & filesystem

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const http = require('http');
+const fs = require('fs');
+
+const PORT = 3000;
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(
-    JSON.stringify({
-      data: 'Hello Node!'
-    })
-  );
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('page/home.html', (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('page/about.html', (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  } else if (req.url === '/create-file') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.writeFile('temp/create.html', '<h1>writing to test file</h1>', err => {
+      if (err) throw err;
+      res.write('file created');
+      res.end();
+    });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    fs.readFile('page/404.html', (err, data) => {
+      if (err) throw err;
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
-server.listen(3000);
+console.log(`Server is running on port ${PORT} as http://localhost:${PORT}`);
+server.listen(PORT);
