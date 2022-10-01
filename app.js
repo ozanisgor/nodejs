0f817e6c4f3f51cdf60f9a64cf337e68a9e4c5e9
ozan isgor <ozanisgor11@gmail.com>
2022-10-01 13:05:39 +0300

    add: express

--- app.js
+++ app.js
@@ -1,38 +1,14 @@
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
 
+const app = express();
 const PORT = 3000;
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('page/home.html', (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === '/about') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('page/about.html', (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      res.end();
-    });
-  } else if (req.url === '/create-file') {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.writeFile('temp/create.html', '<h1>writing to test file</h1>', err => {
-      if (err) throw err;
-      res.write('file created');
-      res.end();
-    });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/html' });
-    fs.readFile('page/404.html', (err, data) => {
-      if (err) throw err;
-      res.write(data);
-      res.end();
-    });
-  }
+
+app.get('/', (req, res) => {
+  res.send('Hello Express!');
 });
 
-console.log(`Server is running on port ${PORT} as http://localhost:${PORT}`);
-server.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
